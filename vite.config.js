7fc369ecbd6a19b66152bd53808df16b21f354ff
andrebilert <abilert@gmail.com>
2025-12-03 14:30:39 -0300

    ajusta vite para github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/landing-page-manoel/', // 👈 nome exato do repositório
+  build: {
+    outDir: 'docs',              // 👈 Vite vai gerar o build direto na pasta docs
+  },
 })
