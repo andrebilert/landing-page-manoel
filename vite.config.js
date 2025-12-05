9b62830b25995ad44784a9ba722f42906df773ff
andrebilert <abilert@gmail.com>
2025-12-05 19:21:16 -0300

    Mudei o vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page-manoel/', // 👈 nome exato do repositório
+  base: '/',
   build: {
-    outDir: 'docs',              // 👈 Vite vai gerar o build direto na pasta docs
+    outDir: 'docs',
   },
 })
